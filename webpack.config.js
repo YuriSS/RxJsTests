bc4e67e2d65df588c32e850cf71ccf3bc0c5002f
YuriSS <yuri.s.dos.santos@gmail.com>
2017-10-17 20:57:45 -0200

    init project

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      },
+      {
+        test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff2"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
+      },
+      {
+        test:    /\.html$/,
+        exclude: /node_modules/,
+        loader:  "file-loader?name=[name].[ext]",
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: { presets: ["es2015"] }
+      }
+    ]
+  }
+};
